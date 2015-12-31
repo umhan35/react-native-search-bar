7a3d500dd0885d63b7f97af3e7daf283ef78285c
ionutlepi <onealepadatescu@gmail.com>
2015-12-31 09:30:58 +0200

    Update RNSearchBarManager.m
    Use the proper context Class for the RN Search bar

--- RNSearchBarManager.m
+++ RNSearchBarManager.m
@@ -62,7 +62,7 @@ RCT_CUSTOM_VIEW_PROPERTY(textFieldBackgroundColor, UIColor, RNSearchBar)
 RCT_CUSTOM_VIEW_PROPERTY(textColor, UIColor, RNSearchBar)
 {
     if([RCTConvert UIColor:json]) {
-       [[UITextField appearanceWhenContainedIn:[UISearchBar class], nil] setDefaultTextAttributes:@{NSForegroundColorAttributeName:[RCTConvert UIColor:json]}];
+       [[UITextField appearanceWhenContainedIn:[RNSearchBar class], nil] setDefaultTextAttributes:@{NSForegroundColorAttributeName:[RCTConvert UIColor:json]}];
     }
 }
 
