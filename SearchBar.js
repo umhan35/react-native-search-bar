7ffa5499d3911057ee49361fc03aa6dd349e347d
Michael Helvey <michael.helvey1@gmail.com>
2016-07-14 16:05:25 -0400

    Fixed default searchText offset, and added unFocus function

--- SearchBar.js
+++ SearchBar.js
@@ -57,6 +57,9 @@ SearchBar = React.createClass({
   focus: function() {
     return NativeModules.RNSearchBarManager.focus(ReactNative.findNodeHandle(this));
   },
+  unFocus: function() {
+    return NativeModules.RNSearchBarManager.unFocus(ReactNative.findNodeHandle(this));
+  },
   render: function() {
     return <RNSearchBar
       style={{height: NativeModules.RNSearchBarManager.ComponentHeight}}
