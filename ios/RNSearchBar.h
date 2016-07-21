8a176fffebec942a042eafeded21f1ab18f733db
Vinay <rmdort@gmail.com>
2016-07-22 00:07:09 +0800

    Added controlled cancel button

--- ios/RNSearchBar.h
+++ ios/RNSearchBar.h
@@ -7,8 +7,11 @@
 
 - (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
 
+<<<<<<< HEAD:ios/RNSearchBar.h
 @property(nonatomic) BOOL _jsShowsCancelButton;
 @property(nonatomic, copy) RCTBubblingEventBlock onSearchButtonPress;
 @property(nonatomic, copy) RCTBubblingEventBlock onCancelButtonPress;
 
+=======
+>>>>>>> Added controlled cancel button:RNSearchBar.h
 @end
