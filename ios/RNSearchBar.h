066a1216397056bceccc3023e121ec3484ba3980
Vinay M <rmdort@gmail.com>
2017-12-29 22:26:32 +0800

    Fixed unmerged change

--- ios/RNSearchBar.h
+++ ios/RNSearchBar.h
@@ -7,11 +7,8 @@
 
 - (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
 
-<<<<<<< HEAD:ios/RNSearchBar.h
 @property(nonatomic) BOOL _jsShowsCancelButton;
 @property(nonatomic, copy) RCTBubblingEventBlock onSearchButtonPress;
 @property(nonatomic, copy) RCTBubblingEventBlock onCancelButtonPress;
 
-=======
->>>>>>> Added controlled cancel button:RNSearchBar.h
 @end
