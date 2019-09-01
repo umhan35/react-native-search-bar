8ca23fc18bed80b7d797c411aff7010c2081a75a
Kyle Roach <kroach.work@gmail.com>
2019-09-01 16:39:11 -0400

    ci: Update eslint toolchain
    Switch to react-native-community eslint

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,12 @@
-// Type definitions for react-native-search-bar
-// Project: https://github.com/umhan35/react-native-search-bar
-// Definitions by: Kyle Roach <https://github.com/iRoachie>
-// TypeScript Version: 2.6
-
-import * as React from 'react'
-import { ReturnKeyType, ReturnKeyTypeIOS, KeyboardType, KeyboardTypeIOS, ViewStyle } from 'react-native'
+import * as React from 'react';
+import {
+  ReturnKeyType,
+  ReturnKeyTypeIOS,
+  KeyboardType,
+  KeyboardTypeIOS,
+  ViewStyle,
+  StyleProp,
+} from 'react-native';
 
 interface Props {
   /**
@@ -12,174 +14,174 @@ interface Props {
    *
    * Default is 'Search'
    */
-  placeholder?: string
+  placeholder?: string;
 
   /**
    * Text in the search bar
    */
-  text?: string
+  text?: string;
 
   /**
    * Background color of the search bar
    */
-  barTintColor?: string
+  barTintColor?: string;
 
   /**
    * Color of the Cancel button, highlight and cursor
    */
-  tintColor?: string
+  tintColor?: string;
 
   /**
    * Textfield text color
    */
-  textColor?: string
+  textColor?: string;
 
   /**
    * Textfield background color
    */
-  textFieldBackgroundColor?: string
+  textFieldBackgroundColor?: string;
 
   /**
    * Toggles whether to show the cancel button or not
    *
    * Default is false
    */
-  showsCancelButton?: boolean
+  showsCancelButton?: boolean;
 
   /**
    * String for the cancel button
-   * 
+   *
    * Default is 'Cancel'
    */
-  cancelButtonText?: string
+  cancelButtonText?: string;
 
   /**
    * Only shows the cancel button while the search bar has focus
-   * 
+   *
    * Default is true
    */
-  showsCancelButtonWhileEditing?: boolean
+  showsCancelButtonWhileEditing?: boolean;
 
   /**
    * Indicates whether the Return key is automatically enabled when the user is entering text.
    *
    * Default is true
    */
-  enablesReturnKeyAutomatically?: boolean
+  enablesReturnKeyAutomatically?: boolean;
 
   /**
    * Indicates whether just to show the text input only
    *
    * Default is false
    */
-  hideBackground?: boolean
+  hideBackground?: boolean;
 
   /**
    * Style of the bar
    *
    * Default is 'default'
    */
-  barStyle?: 'default' | 'black'
+  barStyle?: 'default' | 'black';
 
   /**
    * Style of the search bar
    *
    * Default is 'default'
    */
-  searchBarStyle?: 'default' | 'prominent' | 'minimal'
+  searchBarStyle?: 'default' | 'prominent' | 'minimal';
 
   /**
    * Toggles if the user can interact with the search bar
    *
    * Default is true
    */
-  editable?: boolean
+  editable?: boolean;
 
   /**
    * Return key type for the keyboard
    *
    * Default is 'search'
    */
-  returnKeyType?: ReturnKeyType | ReturnKeyTypeIOS
+  returnKeyType?: ReturnKeyType | ReturnKeyTypeIOS;
 
   /**
    * The type of keyboard to display
-   * 
+   *
    * Default is 'default'
    */
-  keyboardType?: KeyboardType | KeyboardTypeIOS
+  keyboardType?: KeyboardType | KeyboardTypeIOS;
 
   /**
    * The appearance of the keyboard
-   * 
+   *
    * Default is 'default'
    */
-  keyboardAppearance?: 'default' | 'dark' | 'light'
+  keyboardAppearance?: 'default' | 'dark' | 'light';
 
   /**
    * The auto-capitalization behavior
-   * 
+   *
    * Default is 'sentences'
    */
-  autoCapitalize?: 'none' | 'sentences' | 'words' | 'characters'
+  autoCapitalize?: 'none' | 'sentences' | 'words' | 'characters';
 
   /**
    * If autoCorrect is enabled
-   * 
+   *
    * Default is false
    */
-  autoCorrect?: boolean
+  autoCorrect?: boolean;
 
   /**
    * If red underline is shown for misspelt words
-   * 
+   *
    * Default is false
    */
-  spellCheck?: boolean
+  spellCheck?: boolean;
 
   /**
    * Event fired when
    */
-  onChange?(event: { target: number; text: string; eventCount: number }): void
-  
+  onChange?(event: { target: number; text: string; eventCount: number }): void;
+
   /**
    * Searchbar style
    */
-  style?: ViewStyle,
+  style?: StyleProp<ViewStyle>;
 
   /**
    * Event fired when the text in the input changes
    *
    * Returns the current text value in the input
    */
-  onChangeText?(text: string): void
+  onChangeText?(text: string): void;
 
   /**
    * Event fired when the text input is focused
    */
-  onFocus?(): void
+  onFocus?(): void;
 
   /**
    * Event fired the input is blurred
    */
-  onBlur?(): void
+  onBlur?(): void;
 
   /**
    * Event fired when the search button is pressed
    *
    * Returns the current text value in the input
    */
-  onSearchButtonPress?(text: string): void
+  onSearchButtonPress?(text: string): void;
 
   /**
    * Event fired when the cancel button is pressed
    */
-  onCancelButtonPress?(): void
+  onCancelButtonPress?(): void;
 }
 
 export default class SearchBar extends React.Component<Props> {
-  focus(): void
-  blur(): void
-  unFocus(): void
-  clearText(): void
+  focus(): void;
+  blur(): void;
+  unFocus(): void;
+  clearText(): void;
 }
